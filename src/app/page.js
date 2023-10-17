3b2aff3a254e62ddefe6c7a3b8d24773cfbc0115
Anton Prudkohliad <anton.prudkohliad@gmail.com>
2023-10-17 09:29:13 +0200

    Add inputs

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,42 @@
 import MultiSelectDropdown from "../components/MultiSelectDropdown";
 
+const COUNTRIES = [
+  "Austria",
+  "Belgium",
+  "Croatia",
+  "Bulgaria",
+  "Cyprus",
+  "Czech Republic",
+  "Denmark",
+  "Estonia",
+  "Finland",
+  "France",
+  "Germany",
+  "Greece",
+  "Hungary",
+  "Ireland",
+  "Italy",
+  "Latvia",
+  "Lithuania",
+  "Luxembourg",
+  "Malta",
+  "Netherlands",
+  "Poland",
+  "Portugal",
+  "Romania",
+  "Slovakia",
+  "Slovenia",
+  "Spain",
+  "Sweden",
+  "Ukraine",
+];
+
 export default function Page() {
   return (
     <>
       <h1>{"Multi-Select Dropdown"}</h1>
       <form>
-        <MultiSelectDropdown />
+        <MultiSelectDropdown formFieldName={"countries"} options={COUNTRIES} />
         <input type="submit" />
       </form>
     </>
--- src/components/MultiSelectDropdown.jsx
+++ src/components/MultiSelectDropdown.jsx
@@ -1,4 +1,4 @@
-export default function MultiSelectDropdown() {
+export default function MultiSelectDropdown({ formFieldName, options }) {
   return (
     <label className="relative">
       <input type="checkbox" className="hidden peer" />
@@ -7,8 +7,29 @@ export default function MultiSelectDropdown() {
         {"Show the dropdown"}
       </div>
 
-      <div className="absolute bg-white border p-2 transition-opacity opacity-0 pointer-events-none peer-checked:opacity-100 peer-checked:pointer-events-auto">
-        {"Welcome to the dropdown"}
+      <div className="absolute bg-white border transition-opacity opacity-0 pointer-events-none peer-checked:opacity-100 peer-checked:pointer-events-auto">
+        <ul>
+          {options.map((option, i) => {
+            const labelVerticalPaddingClass =
+              i === options.length - 1 ? "py-1" : "pt-1";
+
+            return (
+              <li key={option}>
+                <label
+                  className={`flex whitespace-nowrap cursor-pointer px-2 ${labelVerticalPaddingClass} transition-colors hover:bg-blue-100 [&:has(input:checked)]:bg-blue-200`}
+                >
+                  <input
+                    type="checkbox"
+                    name={formFieldName}
+                    value={option}
+                    className="cursor-pointer"
+                  />
+                  <span className="ml-1">{option}</span>
+                </label>
+              </li>
+            );
+          })}
+        </ul>
       </div>
     </label>
   );
