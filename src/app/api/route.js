15cce712addf870e2d20fd55c887509017c39014
Anton Prudkohliad <anton.prudkohliad@gmail.com>
2023-10-17 12:23:53 +0200

    Show selected countries in the response

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -2,7 +2,7 @@ export async function POST(request) {
   const formData = await request.formData();
   const countries = formData.getAll("countries");
 
-  console.debug("countries", countries);
-
-  return new Response("All good", { status: 200 });
+  return new Response(`Selected countries: ${countries.join(", ")}`, {
+    status: 200,
+  });
 }
