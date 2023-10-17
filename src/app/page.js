4685125d15ac4657362a67d4bb3ea260e0178bbd
Anton Prudkohliad <anton.prudkohliad@gmail.com>
2023-10-17 12:19:41 +0200

    Visual polish

--- src/app/page.js
+++ src/app/page.js
@@ -35,19 +35,24 @@ const COUNTRIES = [
 
 export default function Page() {
   return (
-    <>
-      <h1>{"Multi-Select Dropdown"}</h1>
-      <form action="/api" method="post">
-        <MultiSelectDropdown
-          formFieldName={"countries"}
-          options={COUNTRIES}
-          onChange={(selectedCountries) => {
-            console.debug("selectedCountries", selectedCountries);
-          }}
-          prompt="Select one or more countries"
+    <main className="w-full max-w-xl mx-auto">
+      <h1 className="text-3xl mt-12">{"Multi-Select Dropdown"}</h1>
+      <form action="/api" method="post" className="mt-5">
+        <div>
+          <MultiSelectDropdown
+            formFieldName={"countries"}
+            options={COUNTRIES}
+            onChange={(selectedCountries) => {
+              console.debug("selectedCountries", selectedCountries);
+            }}
+            prompt="Select one or more countries"
+          />
+        </div>
+        <input
+          type="submit"
+          className="bg-blue-500 text-white rounded px-5 py-2 mt-5 cursor-pointer"
         />
-        <input type="submit" />
       </form>
-    </>
+    </main>
   );
 }
