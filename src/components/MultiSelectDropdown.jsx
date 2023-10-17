f7befca4b7a97f46f85615e6bec9e2bb199f52fc
Anton Prudkohliad <anton.prudkohliad@gmail.com>
2023-10-17 09:01:40 +0200

    Add dropdown animations

--- src/components/MultiSelectDropdown.jsx
+++ src/components/MultiSelectDropdown.jsx
@@ -3,11 +3,11 @@ export default function MultiSelectDropdown() {
     <label className="relative">
       <input type="checkbox" className="hidden peer" />
 
-      <div className="cursor-pointer after:content-['▼'] after:text-xs after:ml-1">
+      <div className="cursor-pointer after:content-['▼'] after:text-xs after:ml-1 after:inline-flex after:items-center peer-checked:after:-rotate-180 after:transition-transform">
         {"Show the dropdown"}
       </div>
 
-      <div className="hidden peer-checked:flex absolute bg-white border">
+      <div className="absolute bg-white border p-2 transition-opacity opacity-0 pointer-events-none peer-checked:opacity-100 peer-checked:pointer-events-auto">
         {"Welcome to the dropdown"}
       </div>
     </label>
